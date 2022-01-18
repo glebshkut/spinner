c2785261b73eb212b7c230c741d265f4d013404b
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-18 09:01:31 -0800

    spinner.js

---
+++ spinner.js
@@ -0,0 +1,35 @@
+let delay = 300;
+process.stdout.write('|   ');
+
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r\\   '); 
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r/   ');
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r-   ');
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r\\   '); 
+  }, delay);
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write('\r|   ');
+  }, delay);
+
